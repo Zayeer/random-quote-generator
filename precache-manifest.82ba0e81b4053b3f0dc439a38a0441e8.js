77ed4172929ede3159c4bf4ab4c7c96b022d1e25
Shaik Zayeer Hussain <zayeer.zh@gmail.com>
2020-09-07 14:02:10 +0530

    Updates

---
+++ precache-manifest.82ba0e81b4053b3f0dc439a38a0441e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6b8d78d539bce9c0cbf747c40d0d73",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "f15f21d4f8f628f86265",
+    "url": "/random-quote-generator/static/css/main.53cf472c.chunk.css"
+  },
+  {
+    "revision": "06ed6c223639f9a4e703",
+    "url": "/random-quote-generator/static/js/2.ecd0c60e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/random-quote-generator/static/js/2.ecd0c60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15f21d4f8f628f86265",
+    "url": "/random-quote-generator/static/js/main.71d3cd6b.chunk.js"
+  },
+  {
+    "revision": "efbd8013faa4b47d47b4",
+    "url": "/random-quote-generator/static/js/runtime-main.04b377e6.js"
+  }
+]);
